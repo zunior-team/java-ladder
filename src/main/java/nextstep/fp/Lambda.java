94efa272ec75399b18cf184e16cfb36597abfc8d
Kwanghyun Kim <32362748+nokchax@users.noreply.github.com>
2020-05-25 16:05:15 +0900

    Step1 (#1)
    * test : change anonymous class to lambda
    
    * test : remove duplication using lambda
    
    * feat : map, reduce, filter practice
    
    * feat : map, reduce, filter practice2
    
    * feat : requirement1
    
    * feat : requirement2
    
    * feat : requirement3

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,8 +1,12 @@
 package nextstep.fp;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class Lambda {
+
+    public static final int ZERO = 0;
+
     public static void printAllOld(List<Integer> numbers) {
         System.out.println("printAllOld");
 
@@ -26,31 +30,21 @@ public class Lambda {
         }).start();
     }
 
-    public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+    private static int sum(final List<Integer> numbers, final Function<Integer, Boolean> filter) {
+        return numbers.stream()
+                .filter(filter::apply)
+                .reduce(ZERO, Integer::sum);
     }
 
-    public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAll(final List<Integer> numbers) {
+        return sum(numbers, number -> true);
     }
 
-    public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
-        return total;
+    public static int sumAllEven(final List<Integer> numbers) {
+        return sum(numbers, number -> number % 2 == 0);
+    }
+
+    public static int sumAllOverThree(final List<Integer> numbers) {
+        return sum(numbers, number -> number > 3);
     }
 }
